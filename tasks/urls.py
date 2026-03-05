2c119d8f1b37321a22e52e5fae3db7d19a3a0884
lapwenggan298 <lapwenggan@gmail.com>
2026-03-05 16:39:35 +0800

    Edited app level urls.py
    Modified it to have path/url to create.

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from .views import index, task_list, TaskListView, task_detail, TaskDetailView
 
 urlpatterns = [
     path('', index, name = "index"),
+    path('create', TaskCreateView.as_view(), name = "task-create")
     path('list', TaskListView.as_view(), name = "task-list"), #TaskListView.as_view(), task_list
    #path('list', task_list, name = "task_list"),
     path('<int:pk>', TaskDetailView.as_view(), name = "task_detail"), #task_detail
