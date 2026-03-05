9a41f09573ebebb2329421f3fd46577c688e86ed
lapwenggan298 <lapwenggan@gmail.com>
2026-03-05 18:17:33 +0800

    New File forms.py created If it was already in another commit, my apolgies for the repetition, in that case, updated/modified forms.py Not complete yet as I wasn't able to follow due to the aforementioned difficulties/adjustment to the commands/shortcuts here in nano.
    Content:
    from django import forms
    from .models import necessary models.
    
    "new" class TaskForm(forms.ModelForm)
    content: it has a subclass Meta which I believe handles all the things necessary.
    
    Commented out original class TaskForm(forms.Form)

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,10 +1,20 @@
 from django import forms
-from .models import TaskGroup
+from .models import TaskGroup, Task
 
-class TaskForm(forms.Form):
-    name = forms.CharField(label = "Task Name",max_length=100)
-    due_date = forms.DateTimeField(label = "Due Date")
-    taskgroup = forms.ModelChoiceField(
-        label="Task Group",
-        queryset=TaskGroup.objects.all()
-    )
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+#class TaskForm(forms.Form):
+#    name = forms.CharField(label = "Task Name",max_length=100)
+#    due_date = forms.DateTimeField(
+#        label = "Due Date",
+#        widget=forms.TextInput(
+#            attrs = {'type', 'datetime-local'}
+#        )
+#    )
+#    taskgroup = forms.ModelChoiceField(
+#        label="Task Group",
+#        queryset=TaskGroup.objects.all()
+#    )
