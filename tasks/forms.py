edd41fcd8017d181123cc8839eaa68486329143f
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-03-18 16:29:07 +0800

    Modification to forms.py
    Just added insights as comments and what I remember from the last discussion

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,6 +5,11 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
+	#Model based form, the forms will be the one who will handle all fields, less work compared to below (commented out). 
+	#Since fields = '__all__', it will show all required fields.
+	#Fields can be specified/limited similar to lab#4
+	#You can edit/add widgets so that some forms will take on a different form if I remember correctly from the discussion.
+	#I just don't remember how to edit/do it.
 
 #class TaskForm(forms.Form):
 #    name = forms.CharField(label = "Task Name",max_length=100)
